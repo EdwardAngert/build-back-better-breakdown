6fac6a09a7b7b17c13e11961e5e5f3eea7635d91
Edward Angert <EdwardAngert@users.noreply.github.com>
2021-12-23 15:23:27 -0500

    github action deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://edwardangert.github.io/build-back-better-breakdown',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'EdwardAngert', // Usually your GitHub org/user name.
+  projectName: 'build-back-better-breakdown', // Usually your repo name.
 
   presets: [
     [
